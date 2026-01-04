ca8a1cde40c2d084b73d7db5f5b2bfc765a56fb6
Dallin Huff <me@dallinhuff.com>
2026-01-03 21:07:18 -0700

    md: Add syntax highlighting config for MD files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.dallinhuff.com",
+  markdown: {
+    shikiConfig: {
+      theme: "kanagawa-wave",
+    },
+  },
 });
