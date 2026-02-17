e8b64a88260a6696dbc462a637b6c6f7a4874e7c
Dallin Huff <me@dallinhuff.com>
2026-02-16 22:30:34 -0700

    notes: Add note preview component for home page

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,21 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { rssSchema } from "@astrojs/rss";
 
+const NoteSchema = rssSchema
+  .required({
+    title: true,
+    description: true,
+    pubDate: true,
+  })
+  .extend({
+    draft: z.boolean().default(false),
+  });
+
+export type Note = z.infer<typeof NoteSchema>;
+
 const notes = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/notes" }),
-  schema: rssSchema
-    .required({
-      title: true,
-      description: true,
-      pubDate: true,
-    })
-    .extend({
-      draft: z.boolean().default(false),
-    }),
+  schema: NoteSchema,
 });
 
 export const collections = { notes };
